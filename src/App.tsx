00b701cf4c1e3649e80e910a293aef59a4134f67
xiaofa <xiaofalzx@gmail.com>
2018-12-13 03:20:18 +0800

    update style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React, { Component, Suspense, LazyExoticComponent } from 'react'
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import logo from './logo.svg'
-// import T from './posts/BuildinType.md'
 import './App.css'
 
-// import Tutor from './Tutor'
-// import FAQ from './FAQ'
 import LoadableFAQ from './LoadableFAQ'
 
 const LazyTutor = React.lazy(() => import('./Tutor'))
@@ -14,7 +11,6 @@ function waitingComponent(C: LazyExoticComponent<any>) {
   // const LazyTutor = React.lazy(() => import('./Tutor'))
   return (props: any) => (
     <Suspense fallback={<div>Loading...</div>}>
-      {/* <LazyTutor {...props}></LazyTutor> */}
       <C {...props} />
     </Suspense>
   )
@@ -26,28 +22,34 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            {/* <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p> */}
-						<Link to="/" className="App-link">
-							<img src={logo} className="App-logo" alt="logo"/>
-						</Link>
-            <Link to="/" className="App-link">
-              Home
-            </Link>
-            <Link to="/tutor" className="App-link">
-              Tutor
-            </Link>
-            <Link to="/faq" className="App-link">
-              AsyncFAQ
-            </Link>
+						<div className="header-inner">
+							<span className="App-link-wrapper">
+								<Link to="/">
+									<img src={logo} className="App-logo" alt="logo"/>
+								</Link>
+							</span>
+							<Link to="/" className="App-link">
+								Home
+							</Link>
+							<Link to="/tutor" className="App-link">
+								Tutor
+							</Link>
+							<Link to="/faq" className="App-link">
+								AsyncFAQ
+							</Link>
+							<Link to="/" className="App-link">
+								Resume
+							</Link>
+							<Link to="/" className="App-link">
+								About
+							</Link>
+						</div>
           </header>
-          <div>
+          <main className="App-content">
             <Route path="/" exact={true} />
             <Route path="/tutor" component={waitingComponent(LazyTutor)} />
             <Route path="/faq" component={LoadableFAQ} />
-          </div>
+          </main>
         </div>
       </Router>
     )
