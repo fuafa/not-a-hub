00d166d5a245d456971f88ae080f48f372be7f9a
xiaofa <xiaofalzx@gmail.com>
2019-08-25 08:32:50 +0800

    init rematch

---
+++ src/models/index.ts
@@ -0,0 +1,9 @@
+import { createModel } from '@rematch/core';
+import Post from '../out/PostList.json';
+import Octokit from '@octokit/rest';
+
+// const octokit = new Octokit();
+// octokit.search.issuesAndPullRequests({
+//   q:
+// })
+// export const 
